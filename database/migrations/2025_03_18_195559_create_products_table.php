a06b45f3e64a146f29241b0318a4fdf3e1e98d26
Javier Ditta <jperez428@unab.edu.co>
2025-03-18 15:02:09 -0500

    creacion de productos

--- database/migrations/2025_03_18_195559_create_products_table.php
+++ database/migrations/2025_03_18_195559_create_products_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        $table->string('name');
+        $table->text('description');
+        $table->float('price');
+        $table->string('Category_id');
+        $table->string('items');
+        $table->timestamps();
         });
-    }
+    }   
 
     /**
      * Reverse the migrations.
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('products');
     }
 };
