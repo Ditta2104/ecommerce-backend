e2bdfeb34ab6c8dcaac9b3a61e2a821d954c9cc6
Javier Ditta <jperez428@unab.edu.co>
2025-04-22 14:45:02 -0500

    Se configuró el login

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,14 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', HomeController::class);
+
 
 Route::prefix('/products')->controller(ProductController::class)->group(function(){
     Route::get('', 'index');
     Route::get('/create', 'create');
     Route::get('/show', 'show');
 });
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'welcome']);
